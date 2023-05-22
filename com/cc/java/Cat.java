f8778fbb4050790e2b90a1ef141a729ba9773863
Peter <schmidt.multimedia@gmail.com>
2023-05-22 11:18:04 +0200

    Instanziierung / 2 Objekte

---
+++ com/cc/java/Cat.java
@@ -0,0 +1,13 @@
+package com.cc.java;
+
+public class Cat {
+    
+    // this --> Instanzvariable // Selbstreferenz
+    public void tellYourAddress() {
+        System.out.println("Blick von innen: " + this);
+    }
+
+
+
+
+}

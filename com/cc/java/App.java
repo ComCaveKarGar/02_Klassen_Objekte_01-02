4b93f265bc09510db7e9f74d44c9d25c3c16f230
sven-pke <sven-perschke@gmx.de>
2023-05-23 10:24:47 +0200

    Felder / Wertzuweisung / Typkonvertierung

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -12,12 +12,32 @@ public class App {
         output("Blick von aussen: " + cat1);
         output(cat1.tellYourAddress());
 
+        // Wertzuweisung
+        cat1.name= "Grizabella";
+        cat1.furColor = "white";
+        cat1.age = 29;
+
+        // Ausgabe
+        output(cat1.name);
+        output(cat1.furColor);
+        output(Integer.toString(cat1.age));
+
         // App.output("hi");
         output("-------------------");
 
         Cat cat2 = new Cat();
         output("Blick von aussen: " + cat2);
         output(cat2.tellYourAddress());
+
+        // Wertzuweisung
+        cat2.name= "Alonzo";
+        cat2.furColor = "grey";
+        cat2.age = 35;
+
+        // Ausgabe
+        output(cat2.name);
+        output(cat2.furColor);
+        output(Integer.toString(cat2.age));
     }
  
     public static void output(String outStr) {
--- com/cc/java/Cat.java
+++ com/cc/java/Cat.java
@@ -1,6 +1,17 @@
 package com.cc.java;
 
 public class Cat {
+
+    public String name;
+    public String furColor;
+    public int age;
+
+
+
+
+
+
+    
     
     // this --> Instanzvariable // Selbstreferenz
     public String tellYourAddress() {

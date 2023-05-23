3ca439b93c147b33b289b1d8684dfe1db6f0dcaa
sven-pke <sven-perschke@gmx.de>
2023-05-23 11:33:37 +0200

    Custom Constructor

--- com/cc/java/App.java
+++ com/cc/java/App.java
@@ -8,16 +8,12 @@ public class App {
         // Cat -- Referenztyp
         // cat -- Referenzvariable
         
-        Cat cat1 = new Cat();
-        output("Blick von aussen: " + cat1);
-        output(cat1.tellYourAddress());
+        Cat cat1 = new Cat("Grizabella", "white", 29);
+        // output("Blick von aussen: " + cat1);
+        // output(cat1.tellYourAddress());
 
-        // Wertzuweisung
-        cat1.name= "Grizabella";
-        cat1.furColor = "white";
-        cat1.age = 29;
 
-        // Ausgabe
+        // Ausgabe über Methoden
         output(cat1.tellYourName());
         output(cat1.tellYourFurColor());
         output(Integer.toString(cat1.tellYourAge()));
@@ -25,20 +21,16 @@ public class App {
         // App.output("hi");
         output("-------------------");
 
-        Cat cat2 = new Cat();
-        output("Blick von aussen: " + cat2);
-        output(cat2.tellYourAddress());
+        Cat cat2 = new Cat("Alonzo", "grey", 35);
+        // output("Blick von aussen: " + cat2);
+        // output(cat2.tellYourAddress());
 
-        // Wertzuweisung
-        cat2.name= "Alonzo";
-        cat2.furColor = "grey";
-        cat2.age = 35;
 
         // Ausgabe
         output(cat2.tellYourName());
         output(cat2.tellYourFurColor());
         output(Integer.toString(cat2.tellYourAge()));
-        
+
     }
  
     public static void output(String outStr) {
--- com/cc/java/Cat.java
+++ com/cc/java/Cat.java
@@ -5,6 +5,12 @@ public class Cat {
     public String name;
     public String furColor;
     public int age;
+    
+    public Cat(String name, String furColor, int age) {
+        this.name = name;
+        this.furColor = furColor;
+        this.age = age;
+    }
 
     public String tellYourName() {
         return this.name;

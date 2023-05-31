219fe6e5c470db0d110c30d64600fac95c8ec8d7
Karsten <Moonjumper@gmx.de>
2023-05-31 17:06:56 +0200

    +1

--- com/cc/java/Cat.java
+++ com/cc/java/Cat.java
@@ -2,50 +2,30 @@ package com.cc.java;
 
 public class Cat {
 
-    private String name;
-    private String furColor;
-    private int age;
-    
-    public Cat(String name, String furColor, int age) {
-        this.name = name;
-        this.furColor = furColor;
-        this.age = age;
-    }
+public String name;
+public String furColor;
+public int age; // int ist hier ein primitiv
 
-    // Getter
-    public String getName() {
-        return name;
-    }
+// rechtsklick -> source action -> generate constructor  
 
-    // Setter
-    public void setName(String name) {
-        this.name = name;
-    }
+public Cat(String name, String furColor, int age) {
+    this.name = name;
+    this.furColor = furColor;
+    this.age = age;
+}
 
-    public String getFurColor() {
-        return furColor;
-    }
+// rechtsklick -> source action -> generate getter  
 
-    public void setFurColor(String furColor) {
-        this.furColor = furColor;
-    }
+public String getName() {
+    return name;
+}
 
-    public int getAge() {
-        return age;
-    }
+public String getFurColor() {
+    return furColor;
+}
 
-    public void setAge(int age) {
-        this.age = age;
-    }
-
-
-
-
-
-
-
-
-
-
+public int getAge() { 
+    return age;
+}
 
 }
